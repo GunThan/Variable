09d37ca9b69d8d70a6355865b70cb45d617db1d9
GunThan <Minhdentranbinh@gamil.com>
2021-11-01 14:02:52 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+/**
+ * 
+ * @author TranHuuBinhMinh
+ *
+ */
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		/**
+		 * Khai bao bien
+		 */
+		String firstName = "Tran Huu Binh";
+		String lastName = "Minh";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println(" Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+}
